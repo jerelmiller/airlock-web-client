9de2e91bdcebf3a4122bf4322ae097315ee1e4b6
Jerel Miller <jerelmiller@gmail.com>
2024-08-20 16:12:04 -0600

    Convert QueryResult to TS

---
+++ src/components/QueryResult.tsx
@@ -0,0 +1,34 @@
+import { Center, Spinner } from "@chakra-ui/react";
+import { ApolloError } from "@apollo/client";
+
+interface QueryResultProps<TData> {
+  loading: boolean;
+  error: ApolloError | undefined;
+  data: TData;
+  children: (data: TData) => JSX.Element;
+}
+
+export default function QueryResult<TData = unknown>({
+  loading,
+  error,
+  data,
+  children,
+}: QueryResultProps<TData>) {
+  if (loading) {
+    return (
+      <Center minH="100vh">
+        <Spinner size="lg" />
+      </Center>
+    );
+  }
+
+  if (error) {
+    return <Center>uhoh error! {error.message}</Center>;
+  }
+
+  if (data) {
+    return children(data);
+  }
+
+  return <Center>Nothing to show</Center>;
+}
