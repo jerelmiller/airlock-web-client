da7fa88f433371bbf0bc327f58fcc863d564ad9d
Jerel Miller <jerelmiller@gmail.com>
2024-08-20 17:09:29 -0600

    Update to createBrowserRouter and RouterProvider

--- src/App.tsx
+++ src/App.tsx
@@ -13,56 +13,60 @@ import {
   Trips,
   Wallet,
 } from "./pages";
-import { useUser } from "./utils";
 
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
 export default function App() {
-  const { user } = useUser();
-
-  return (
-    <Router>
-      <Routes>
-        <Route path="/search">
-          <Search />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/listings">
-          <Listings />
-        </Route>
-        <Route path="/listings/create">
-          <CreateListing />
-        </Route>
-        <Route exact path="/listing/:id">
-          <Listing />
-        </Route>
-        <Route path="/listing/:id/edit">
-          <EditListing />
-        </Route>
-        <Route path="/listing/:id/bookings">
-          <HostBookings />
-        </Route>
-        <Route path="/listing/:id/past-bookings">
-          <HostPastBookings />
-        </Route>
-        <Route path="/trips">
-          <Trips />
-        </Route>
-        <Route path="/past-trips">
-          <PastTrips />
-        </Route>
-        <Route user={user} path="/profile">
-          <Profile />
-        </Route>
-        <Route user={user} path="/wallet">
-          <Wallet />
-        </Route>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  return <RouterProvider router={router} />;
 }
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/search">
+        <Search />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/listings">
+        <Listings />
+      </Route>
+      <Route path="/listings/create">
+        <CreateListing />
+      </Route>
+      <Route exact path="/listing/:id">
+        <Listing />
+      </Route>
+      <Route path="/listing/:id/edit">
+        <EditListing />
+      </Route>
+      <Route path="/listing/:id/bookings">
+        <HostBookings />
+      </Route>
+      <Route path="/listing/:id/past-bookings">
+        <HostPastBookings />
+      </Route>
+      <Route path="/trips">
+        <Trips />
+      </Route>
+      <Route path="/past-trips">
+        <PastTrips />
+      </Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
+      <Route path="/wallet">
+        <Wallet />
+      </Route>
+      <Route exact path="/">
+        <Home />
+      </Route>
+    </>
+  )
+);
