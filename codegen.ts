8098d703cae7553fdf165f7cb753c4b485fa999a
Jerel Miller <jerelmiller@gmail.com>
2024-08-22 09:46:46 -0600

    Update codegen config

--- codegen.ts
+++ codegen.ts
@@ -2,10 +2,30 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://rt-airlock-router.herokuapp.com/",
+  schema: "./src/schema.graphql",
   documents: "src/**/*.tsx",
+  hooks: {
+    afterAllFileWrite: ["prettier --write"],
+  },
   generates: {
-    "./src/__generated__/types.ts": {},
+    "./src/__generated__/types.ts": {
+      plugins: ["typescript", "typescript-operations"],
+      config: {
+        avoidOptionals: {
+          field: true,
+          inputValue: false,
+          object: false,
+          defaultValue: false,
+        },
+        dedupeOperationSuffix: true,
+        defaultScalarType: "unknown",
+        omitOperationSuffix: true,
+        skipTypeNameForRoot: true,
+        namingConvention: {
+          typeNames: "keep",
+        },
+      },
+    },
   },
 };
 
