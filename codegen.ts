bfea2428902175b1b619051103fa6ce243a83033
Jerel Miller <jerelmiller@gmail.com>
2024-08-22 11:17:49 -0600

    Look for GraphQL sources in ts and tsx files

--- codegen.ts
+++ codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 const config: CodegenConfig = {
   overwrite: true,
   schema: "./src/schema.graphql",
-  documents: "src/**/*.tsx",
+  documents: "src/**/*.{ts,tsx}",
   hooks: {
     afterAllFileWrite: ["prettier --write"],
   },
