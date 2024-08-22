8a63d6ad9181ec780b937bb03d3ac2d8077728fc
Jerel Miller <jerelmiller@gmail.com>
2024-08-22 09:35:45 -0600

    Update URL used for GraphQL codegen

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "http://localhost:4000",
+  schema: "https://rt-airlock-router.herokuapp.com/",
   documents: "src/**/*.tsx",
   generates: {
     "./src/__generated__/types.ts": {},
