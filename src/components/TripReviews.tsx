464f739560bbf8ffed9d4dab55bb3ab75e37b747
Jerel Miller <jerelmiller@gmail.com>
2024-08-26 21:46:05 -0600

    Create and export a GuestReview component from TripReviews

--- src/components/TripReviews.tsx
+++ src/components/TripReviews.tsx
@@ -11,10 +11,11 @@ import {
   VStack,
 } from "@chakra-ui/react";
 import { IoChevronDown, IoChevronUp } from "react-icons/io5";
+import * as GraphQLTypes from "../__generated__/types";
 
 interface ReviewType {
-  rating: number | null;
-  text: string | null;
+  rating: number;
+  text: string;
 }
 
 interface ReviewProps {
@@ -216,4 +217,95 @@ export default function TripReviews({
       {isHost ? renderHostView() : renderGuestView()}
     </VStack>
   );
+}
+
+interface GuestReviewProps {
+  locationReview: GraphQLTypes.Review | null;
+  hostReview: GraphQLTypes.Review | null;
+  guestReview: GraphQLTypes.Review | null;
+  onSubmitReview: (review: ReviewType) => void;
+}
+
+interface ReviewFormType {
+  text: string;
+  rating: number | null;
+}
+
+export function GuestReview({
+  locationReview,
+  hostReview,
+  guestReview,
+  onSubmitReview,
+}: GuestReviewProps) {
+  const [isReviewInputOpen, setIsReviewInputOpen] = useState(false);
+  const [review, setReview] = useState<ReviewFormType>({
+    rating: null,
+    text: "",
+  });
+
+  return (
+    <VStack w="full" alignItems="flex-start" spacing="3" flex="1">
+      <Heading as="h2" fontWeight="semibold" fontSize="lg">
+        What your guest had to say
+      </Heading>
+      {!locationReview && !hostReview && <NoReviews author="guest" />}
+      {locationReview && <Review review={locationReview}>Location</Review>}
+      {hostReview && <Review review={hostReview}>Host</Review>}
+      {guestReview ? (
+        <>
+          <Heading as="h2" fontWeight="semibold" fontSize="lg">
+            Your rating and review
+          </Heading>
+          <Review review={guestReview}>Guest</Review>
+        </>
+      ) : (
+        <>
+          <Button
+            mt={4}
+            variant="link"
+            rightIcon={isReviewInputOpen ? <IoChevronUp /> : <IoChevronDown />}
+            onClick={() => setIsReviewInputOpen((prevState) => !prevState)}
+          >
+            Review your guest
+          </Button>
+          <Collapse in={isReviewInputOpen} py="4" w="100%">
+            <Stack spacing={4}>
+              <Heading as="h2" fontWeight="semibold" fontSize="lg">
+                Your rating and review
+              </Heading>
+              <ReviewInput label="Guest" review={review} onChange={setReview} />
+              <Button
+                onClick={() => {
+                  if (isFullReview(review)) {
+                    onSubmitReview(review);
+                  }
+                }}
+                disabled={!isFullReview(review)}
+                w="fit-content"
+              >
+                Submit Review
+              </Button>
+            </Stack>
+          </Collapse>
+        </>
+      )}
+    </VStack>
+  );
+}
+
+interface NoReviewsProps {
+  author: string;
+}
+
+function NoReviews({ author }: NoReviewsProps) {
+  return (
+    <Text>
+      Your {author} hasn&apos;t reviewed their stay yet. We&apos;ve reached out
+      to them and will report back here once we&apos;ve received their thoughts.
+    </Text>
+  );
+}
+
+function isFullReview(review: ReviewFormType): review is ReviewType {
+  return Boolean(review.rating && review.text);
 }
