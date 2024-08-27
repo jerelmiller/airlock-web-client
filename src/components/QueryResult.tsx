272d273ebdf621281aaa6e23d132a5ce0a18e80e
Jerel Miller <jerelmiller@gmail.com>
2024-08-26 18:10:58 -0600

    Allow null in QueryResult

--- src/components/QueryResult.tsx
+++ src/components/QueryResult.tsx
@@ -5,7 +5,7 @@ interface QueryResultProps<TData> {
   loading: boolean;
   error: ApolloError | undefined;
   data: TData;
-  children: (data: NonNullable<TData>) => JSX.Element;
+  children: (data: NonNullable<TData>) => JSX.Element | null;
 }
 
 export default function QueryResult<TData = unknown>({
