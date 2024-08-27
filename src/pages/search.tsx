05669ca0e0f49acef1eb7ef1fb52b6ae98b52387
Jerel Miller <jerelmiller@gmail.com>
2024-08-26 23:01:03 -0600

    Convert to useBackgroundQuery in search

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,7 @@
 import BedroomInput from "../components/BedroomInput";
 import Layout from "../layouts/Layout";
 import ListingCell from "../components/ListingCell";
-import QueryResult from "../components/QueryResult";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import format from "date-fns/format";
 import {
   Box,
@@ -16,7 +15,13 @@ import {
   Text,
   VStack,
 } from "@chakra-ui/react";
-import { gql, TypedDocumentNode, useQuery } from "@apollo/client";
+import {
+  gql,
+  QueryRef,
+  TypedDocumentNode,
+  useBackgroundQuery,
+  useReadQuery,
+} from "@apollo/client";
 import { useLocation } from "react-router-dom";
 
 import "react-datepicker/dist/react-datepicker.css";
@@ -26,6 +31,9 @@ import {
 } from "./__generated__/search.types";
 import { SortByCriteria } from "../__generated__/types";
 import { DatePickerInput } from "../components/DatePickerInput";
+import { PageSpinner } from "../components/PageSpinner";
+import { ErrorBoundary } from "react-error-boundary";
+import { PageError } from "../components/PageError";
 
 export const SEARCH_LISTINGS: TypedDocumentNode<
   SearchListingsQuery,
@@ -44,6 +52,7 @@ export const SEARCH_LISTINGS: TypedDocumentNode<
     }
   }
 `;
+
 export default function Search() {
   function useSearchQuery() {
     return new URLSearchParams(useLocation().search);
@@ -60,9 +69,8 @@ export default function Search() {
   const [numOfBeds, setNumOfBeds] = useState(numOfBedsFromUrl);
   const [sortBy, setSortBy] = useState(SortByCriteria.COST_ASC);
   const [page, setPage] = useState(1);
-  const [nextPageButtonDisabled, setNextPageButtonDisabled] = useState(false);
 
-  const { loading, error, data, fetchMore } = useQuery(SEARCH_LISTINGS, {
+  const [queryRef] = useBackgroundQuery(SEARCH_LISTINGS, {
     variables: {
       searchListingsInput: {
         checkInDate: checkInDate.toISOString(),
@@ -74,14 +82,6 @@ export default function Search() {
       },
     },
   });
-
-  useEffect(() => {
-    if (data?.searchListings?.length === 0) {
-      const newPage = page - 1;
-      setPage(newPage);
-      setNextPageButtonDisabled(true);
-    }
-  }, [data?.searchListings?.length, page, fetchMore]);
 
   return (
     <Layout>
@@ -144,76 +144,119 @@ export default function Search() {
         </Stack>
       </Center>
       <Divider borderWidth="1px" />
-      <QueryResult loading={loading} error={error} data={data}>
-        {(data) => {
-          return (
-            <Stack mb="8" p={12} pt={9}>
-              <Flex
-                alignItems="center"
-                justifyContent="space-between"
-                mb="4"
-                flexWrap="wrap"
-              >
-                <Heading as="h2" fontSize="3xl">
-                  Stays across space
-                </Heading>
-                <Flex alignItems="center" flexWrap="wrap">
-                  <Text fontWeight="bold" fontSize="lg" mr={4}>
-                    Sort by
-                  </Text>
-                  <Select
-                    width="200px"
-                    size="lg"
-                    onChange={(e) => {
-                      setSortBy(e.target.value as SortByCriteria);
-                      setPage(1);
-                    }}
-                    value={sortBy}
-                  >
-                    <option disabled>Sort by</option>
-                    <option value="COST_ASC">Price (low to high)</option>
-                    <option value="COST_DESC">Price (high to low)</option>
-                  </Select>
-                </Flex>
-              </Flex>
-              {data.searchListings.length > 0 ? (
-                <VStack spacing="4">
-                  {data.searchListings.filter(Boolean).map((listingData) => (
-                    <ListingCell
-                      key={listingData.title}
-                      listing={listingData}
-                      to={`/listing/${listingData.id}/?startDate=${format(
-                        checkInDate,
-                        "MM-dd-yyyy",
-                      )}&endDate=${format(checkOutDate, "MM-dd-yyyy")}`}
-                    />
-                  ))}
-                </VStack>
-              ) : (
-                <Heading size="lg">No results found.</Heading>
-              )}
-
-              <Flex justifyContent="space-between">
-                <Button
-                  onClick={async () => {
-                    setPage(page - 1);
-                    setNextPageButtonDisabled(false);
-                  }}
-                  isDisabled={page === 1}
-                >
-                  Previous page
-                </Button>
-                <Button
-                  onClick={() => setPage(page + 1)}
-                  isDisabled={nextPageButtonDisabled}
-                >
-                  Next page
-                </Button>
-              </Flex>
-            </Stack>
-          );
-        }}
-      </QueryResult>
+      <Suspense fallback={<PageSpinner />}>
+        <ErrorBoundary
+          fallbackRender={({ error }) => <PageError error={error} />}
+        >
+          <SearchResults
+            queryRef={queryRef}
+            page={page}
+            checkInDate={checkInDate}
+            checkOutDate={checkOutDate}
+            sortBy={sortBy}
+            onChangePage={setPage}
+            onChangeSort={setSortBy}
+          />
+        </ErrorBoundary>
+      </Suspense>
     </Layout>
+  );
+}
+
+interface SearchResultsProps {
+  queryRef: QueryRef<SearchListingsQuery, SearchListingsQueryVariables>;
+  page: number;
+  sortBy: SortByCriteria;
+  checkInDate: Date;
+  checkOutDate: Date;
+  onChangePage: (page: number) => void;
+  onChangeSort: (sort: SortByCriteria) => void;
+}
+
+function SearchResults({
+  queryRef,
+  checkInDate,
+  checkOutDate,
+  page,
+  sortBy,
+  onChangePage,
+  onChangeSort,
+}: SearchResultsProps) {
+  const { data } = useReadQuery(queryRef);
+  const [nextPageButtonDisabled, setNextPageButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    if (data.searchListings.length === 0) {
+      const newPage = page - 1;
+      onChangePage(newPage);
+      setNextPageButtonDisabled(true);
+    }
+  }, [data.searchListings.length, page, onChangePage]);
+
+  return (
+    <Stack mb="8" p={12} pt={9}>
+      <Flex
+        alignItems="center"
+        justifyContent="space-between"
+        mb="4"
+        flexWrap="wrap"
+      >
+        <Heading as="h2" fontSize="3xl">
+          Stays across space
+        </Heading>
+        <Flex alignItems="center" flexWrap="wrap">
+          <Text fontWeight="bold" fontSize="lg" mr={4}>
+            Sort by
+          </Text>
+          <Select
+            width="200px"
+            size="lg"
+            onChange={(e) => {
+              onChangeSort(e.target.value as SortByCriteria);
+              onChangePage(1);
+            }}
+            value={sortBy}
+          >
+            <option disabled>Sort by</option>
+            <option value="COST_ASC">Price (low to high)</option>
+            <option value="COST_DESC">Price (high to low)</option>
+          </Select>
+        </Flex>
+      </Flex>
+      {data.searchListings.length > 0 ? (
+        <VStack spacing="4">
+          {data.searchListings.filter(Boolean).map((listingData) => (
+            <ListingCell
+              key={listingData.title}
+              listing={listingData}
+              to={`/listing/${listingData.id}/?startDate=${format(
+                checkInDate,
+                "MM-dd-yyyy",
+              )}&endDate=${format(checkOutDate, "MM-dd-yyyy")}`}
+            />
+          ))}
+        </VStack>
+      ) : (
+        <Heading size="lg">No results found.</Heading>
+      )}
+
+      <Flex justifyContent="space-between">
+        <Button
+          onClick={async () => {
+            onChangePage(page - 1);
+            setNextPageButtonDisabled(false);
+          }}
+          isDisabled={page === 1}
+        >
+          Previous page
+        </Button>
+        <Button
+          onClick={() => onChangePage(page + 1)}
+          isDisabled={nextPageButtonDisabled}
+        >
+          Next page
+        </Button>
+      </Flex>
+    </Stack>
   );
 }
