5da21a02a525dfe36f3c965da454605ba667003a
Jerel Miller <jerelmiller@gmail.com>
2024-08-28 16:10:43 -0600

    Move routes to router.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,6 @@
-import Layout from "./layouts/Layout";
-import {
-  CreateListing,
-  EditListing,
-  Home,
-  HostBookings,
-  HostPastBookings,
-  Listing,
-  Listings,
-  Login,
-  PastTrips,
-  Profile,
-  Search,
-  Trips,
-  Wallet,
-} from "./pages";
-
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 export default function App() {
   return <RouterProvider router={router} />;
 }
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route element={<Layout />}>
-        <Route path="/search" element={<Search />} />
-        <Route path="/listings" element={<Listings />} />
-        <Route path="/listings/create" element={<CreateListing />} />
-        <Route path="/listing/:id" element={<Listing />} />
-        <Route path="/listing/:id/edit" element={<EditListing />} />
-        <Route path="/listing/:id/bookings" element={<HostBookings />} />
-        <Route
-          path="/listing/:id/past-bookings"
-          element={<HostPastBookings />}
-        />
-        <Route path="/trips" element={<Trips />} />
-        <Route path="/past-trips" element={<PastTrips />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/wallet" element={<Wallet />} />
-      </Route>
-    </>
-  )
-);
---
+++ src/router.tsx
@@ -0,0 +1,47 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+import Layout from "./layouts/Layout";
+import {
+  CreateListing,
+  EditListing,
+  Home,
+  HostBookings,
+  HostPastBookings,
+  Listing,
+  Listings,
+  Login,
+  PastTrips,
+  Profile,
+  Search,
+  Trips,
+  Wallet,
+} from "./pages";
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route element={<Layout />}>
+        <Route path="/search" element={<Search />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/listings/create" element={<CreateListing />} />
+        <Route path="/listing/:id" element={<Listing />} />
+        <Route path="/listing/:id/edit" element={<EditListing />} />
+        <Route path="/listing/:id/bookings" element={<HostBookings />} />
+        <Route
+          path="/listing/:id/past-bookings"
+          element={<HostPastBookings />}
+        />
+        <Route path="/trips" element={<Trips />} />
+        <Route path="/past-trips" element={<PastTrips />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/wallet" element={<Wallet />} />
+      </Route>
+    </>
+  )
+);
