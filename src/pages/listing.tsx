833de3f34099cedcff0c08e8c0c025bac57efa89
Jerel Miller <jerelmiller@gmail.com>
2024-08-28 16:53:39 -0600

    Use .filter when looping over reviews

--- src/pages/listing.tsx
+++ src/pages/listing.tsx
@@ -243,30 +243,27 @@ export default function Listings() {
               {reviews.length === 0 ? (
                 <Text>Uh-oh, this place has no reviews yet!</Text>
               ) : (
-                reviews.map(
-                  (review) =>
-                    review && (
-                      <Flex align="flex-start" key={review.author.id}>
-                        <Stack>
-                          <Avatar
-                            name="profile"
-                            size="md"
-                            borderColor="white"
-                            borderWidth="1px"
-                            src={review.author.profilePicture}
-                            bg="gray.50"
-                          />
-                        </Stack>
-                        <Stack direction="column" spacing="1" pl={4}>
-                          <HStack align="flex-start">
-                            <Heading size="sm">{review.author.name}</Heading>
-                            <Stars size={16} rating={review.rating} />
-                          </HStack>
-                          <Text>{review.text}</Text>
-                        </Stack>
-                      </Flex>
-                    ),
-                )
+                reviews.filter(Boolean).map((review) => (
+                  <Flex align="flex-start" key={review.author.id}>
+                    <Stack>
+                      <Avatar
+                        name="profile"
+                        size="md"
+                        borderColor="white"
+                        borderWidth="1px"
+                        src={review.author.profilePicture}
+                        bg="gray.50"
+                      />
+                    </Stack>
+                    <Stack direction="column" spacing="1" pl={4}>
+                      <HStack align="flex-start">
+                        <Heading size="sm">{review.author.name}</Heading>
+                        <Stars size={16} rating={review.rating} />
+                      </HStack>
+                      <Text>{review.text}</Text>
+                    </Stack>
+                  </Flex>
+                ))
               )}
             </Stack>
           </Box>
