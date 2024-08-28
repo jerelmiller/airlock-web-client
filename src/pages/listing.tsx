b934ca1d7b1c78433c5f45900225c9a6a275615c
Jerel Miller <jerelmiller@gmail.com>
2024-08-28 16:49:50 -0600

    Switch to useSuspenseQuery for listing

--- src/pages/listing.tsx
+++ src/pages/listing.tsx
@@ -1,6 +1,5 @@
 import BookStay from "../components/BookStay";
 import LocationType from "../components/LocationType";
-import QueryResult from "../components/QueryResult";
 import Stars from "../components/Stars";
 import startCase from "lodash/startCase";
 
@@ -8,6 +7,7 @@ import {
   Avatar,
   Box,
   Button,
+  Center,
   Divider,
   Flex,
   HStack,
@@ -20,7 +20,7 @@ import {
 import { GUEST_TRIPS } from "./trips";
 import { IoBedOutline, IoCreate } from "react-icons/io5";
 import { Link, useParams } from "react-router-dom";
-import { gql, TypedDocumentNode, useQuery } from "@apollo/client";
+import { gql, TypedDocumentNode, useSuspenseQuery } from "@apollo/client";
 import { useUser } from "../utils";
 import {
   GetListingDetailsQuery,
@@ -98,198 +98,188 @@ function AmenityList({ amenities, category }: AmenityListProps) {
 }
 
 export default function Listings() {
-  const { id } = useParams();
+  const { id: idParam } = useParams();
   const { user } = useUser();
-  const { loading, error, data } = useQuery(LISTING, {
-    variables: { id: id! },
-  });
+  const { data } = useSuspenseQuery(LISTING, { variables: { id: idParam! } });
+
+  if (!data.listing) {
+    return <Center>Listing not found</Center>;
+  }
+
+  const {
+    id,
+    title,
+    description,
+    numOfBeds,
+    locationType,
+    photoThumbnail,
+    amenities,
+    host,
+    reviews,
+    overallRating,
+    costPerNight,
+    bookings,
+  } = data.listing;
+
+  const amenitiesByCategory = amenities
+    .filter(Boolean)
+    .reduce<Record<string, string[]>>((acc, amenity) => {
+      if (acc[amenity.category]) {
+        acc[amenity.category] = [...acc[amenity.category], amenity.name];
+      } else {
+        acc[amenity.category] = [amenity.name];
+      }
+      return acc;
+    }, {});
 
   return (
-    <QueryResult loading={loading} error={error} data={data}>
-      {(data) => {
-        if (!data.listing) {
-          return <></>;
-        }
-
-        const {
-          id,
-          title,
-          description,
-          numOfBeds,
-          locationType,
-          photoThumbnail,
-          amenities,
-          host,
-          reviews,
-          overallRating,
-          costPerNight,
-          bookings,
-        } = data.listing;
-
-        const amenitiesByCategory = amenities
-          .filter(Boolean)
-          .reduce<Record<string, string[]>>((acc, amenity) => {
-            if (acc[amenity.category]) {
-              acc[amenity.category] = [...acc[amenity.category], amenity.name];
-            } else {
-              acc[amenity.category] = [amenity.name];
-            }
-            return acc;
-          }, {});
-
-        return (
-          <Stack direction="column" mb="12" spacing="6">
-            <Flex justifyContent="space-between">
-              <Stack>
-                <Heading as="h1" size="lg">
-                  {title}
-                </Heading>
-                {overallRating ? (
-                  <Stars size={20} rating={overallRating} />
-                ) : (
-                  <Text>Uh-oh, this place has no reviews yet!</Text>
-                )}
-              </Stack>
-              {host && user && host.id === user.id && (
-                <Button
-                  as={Link}
-                  to={`/listing/${id}/edit`}
-                  rightIcon={<IoCreate size={20} />}
-                  ml="4"
-                >
-                  Edit your listing
-                </Button>
-              )}
-            </Flex>
-            <Image
-              src={photoThumbnail}
-              alt={title}
-              objectFit="cover"
-              width="100%"
-              borderRadius={8}
-            />
-            <Flex direction="row" flexWrap="wrap">
-              <Stack flex="1" direction="column" spacing="6" mr={8}>
-                <Stack spacing="4">
-                  <Heading as="h2" size="md">
-                    Details
-                  </Heading>
-                  <HStack spacing="16" align="top">
-                    <Stack spacing="2">
-                      <Text fontWeight="bold">Number of beds</Text>
-                      <Stack align="center" direction="row">
-                        <IoBedOutline size={22} />
-                        <Text ml="1">{numOfBeds} beds</Text>
-                      </Stack>
-                    </Stack>
-                    <Stack spacing="2">
-                      <Text fontWeight="bold">Location Type </Text>
-                      <Stack align="center" direction="row">
-                        <LocationType locType={locationType} size="22px" />
-                        <Text casing="capitalize" ml="1">
-                          {locationType}
-                        </Text>
-                      </Stack>
-                    </Stack>
-                  </HStack>
+    <Stack direction="column" mb="12" spacing="6">
+      <Flex justifyContent="space-between">
+        <Stack>
+          <Heading as="h1" size="lg">
+            {title}
+          </Heading>
+          {overallRating ? (
+            <Stars size={20} rating={overallRating} />
+          ) : (
+            <Text>Uh-oh, this place has no reviews yet!</Text>
+          )}
+        </Stack>
+        {host && user && host.id === user.id && (
+          <Button
+            as={Link}
+            to={`/listing/${id}/edit`}
+            rightIcon={<IoCreate size={20} />}
+            ml="4"
+          >
+            Edit your listing
+          </Button>
+        )}
+      </Flex>
+      <Image
+        src={photoThumbnail}
+        alt={title}
+        objectFit="cover"
+        width="100%"
+        borderRadius={8}
+      />
+      <Flex direction="row" flexWrap="wrap">
+        <Stack flex="1" direction="column" spacing="6" mr={8}>
+          <Stack spacing="4">
+            <Heading as="h2" size="md">
+              Details
+            </Heading>
+            <HStack spacing="16" align="top">
+              <Stack spacing="2">
+                <Text fontWeight="bold">Number of beds</Text>
+                <Stack align="center" direction="row">
+                  <IoBedOutline size={22} />
+                  <Text ml="1">{numOfBeds} beds</Text>
                 </Stack>
-                <Divider />
-                <Stack>
-                  <Heading as="h2" size="md">
-                    About this location
-                  </Heading>
-                  <Text fontSize="lg" fontWeight="regular" mr="1">
-                    {description}
+              </Stack>
+              <Stack spacing="2">
+                <Text fontWeight="bold">Location Type </Text>
+                <Stack align="center" direction="row">
+                  <LocationType locType={locationType} size="22px" />
+                  <Text casing="capitalize" ml="1">
+                    {locationType}
                   </Text>
                 </Stack>
-                <Divider />
-                <Box>
-                  <Heading as="h2" size="md" mb="2">
-                    Amenities
-                  </Heading>
-                  <Stack spacing="3">
-                    {Object.entries(amenitiesByCategory).map(([key, value]) => (
-                      <AmenityList category={key} amenities={value} key={key} />
-                    ))}
-                  </Stack>
-                </Box>
-                <Divider />
-                <Box>
-                  <Heading as="h2" size="md" mb="2">
-                    Meet your host
-                  </Heading>
-                  <Flex align="flex-start" justify="space-between">
-                    <Stack>
-                      <Flex>
-                        <Text fontWeight="semibold" mr={4}>
-                          {host.name}
-                        </Text>
-                        <Stars size={16} rating={host.overallRating ?? 0} />
-                      </Flex>
-                      <Text>{host.profileDescription}</Text>
-                    </Stack>
-                    <Avatar
-                      name="profile"
-                      size="md"
-                      borderColor="white"
-                      borderWidth="1px"
-                      src={host.profilePicture}
-                      ml={4}
-                      bg="gray.50"
-                    />
-                  </Flex>
-                </Box>
-                <Divider />
-                <Box>
-                  <Heading as="h2" size="md" mb="6">
-                    What other space travelers have to say about this stay
-                  </Heading>
-                  <Stack direction="column" spacing="6">
-                    {reviews.length === 0 ? (
-                      <Text>Uh-oh, this place has no reviews yet!</Text>
-                    ) : (
-                      reviews.map(
-                        (review) =>
-                          review && (
-                            <Flex align="flex-start" key={review.author.id}>
-                              <Stack>
-                                <Avatar
-                                  name="profile"
-                                  size="md"
-                                  borderColor="white"
-                                  borderWidth="1px"
-                                  src={review.author.profilePicture}
-                                  bg="gray.50"
-                                />
-                              </Stack>
-                              <Stack direction="column" spacing="1" pl={4}>
-                                <HStack align="flex-start">
-                                  <Heading size="sm">
-                                    {review.author.name}
-                                  </Heading>
-                                  <Stars size={16} rating={review.rating} />
-                                </HStack>
-                                <Text>{review.text}</Text>
-                              </Stack>
-                            </Flex>
-                          ),
-                      )
-                    )}
-                  </Stack>
-                </Box>
               </Stack>
-
-              <BookStay
-                costPerNight={costPerNight}
-                bookings={bookings.filter(Boolean)}
-                listingId={id}
-                refetchQueries={[LISTING, { query: GUEST_TRIPS }]}
-                userRole={user?.__typename}
+            </HStack>
+          </Stack>
+          <Divider />
+          <Stack>
+            <Heading as="h2" size="md">
+              About this location
+            </Heading>
+            <Text fontSize="lg" fontWeight="regular" mr="1">
+              {description}
+            </Text>
+          </Stack>
+          <Divider />
+          <Box>
+            <Heading as="h2" size="md" mb="2">
+              Amenities
+            </Heading>
+            <Stack spacing="3">
+              {Object.entries(amenitiesByCategory).map(([key, value]) => (
+                <AmenityList category={key} amenities={value} key={key} />
+              ))}
+            </Stack>
+          </Box>
+          <Divider />
+          <Box>
+            <Heading as="h2" size="md" mb="2">
+              Meet your host
+            </Heading>
+            <Flex align="flex-start" justify="space-between">
+              <Stack>
+                <Flex>
+                  <Text fontWeight="semibold" mr={4}>
+                    {host.name}
+                  </Text>
+                  <Stars size={16} rating={host.overallRating ?? 0} />
+                </Flex>
+                <Text>{host.profileDescription}</Text>
+              </Stack>
+              <Avatar
+                name="profile"
+                size="md"
+                borderColor="white"
+                borderWidth="1px"
+                src={host.profilePicture}
+                ml={4}
+                bg="gray.50"
               />
             </Flex>
-          </Stack>
-        );
-      }}
-    </QueryResult>
+          </Box>
+          <Divider />
+          <Box>
+            <Heading as="h2" size="md" mb="6">
+              What other space travelers have to say about this stay
+            </Heading>
+            <Stack direction="column" spacing="6">
+              {reviews.length === 0 ? (
+                <Text>Uh-oh, this place has no reviews yet!</Text>
+              ) : (
+                reviews.map(
+                  (review) =>
+                    review && (
+                      <Flex align="flex-start" key={review.author.id}>
+                        <Stack>
+                          <Avatar
+                            name="profile"
+                            size="md"
+                            borderColor="white"
+                            borderWidth="1px"
+                            src={review.author.profilePicture}
+                            bg="gray.50"
+                          />
+                        </Stack>
+                        <Stack direction="column" spacing="1" pl={4}>
+                          <HStack align="flex-start">
+                            <Heading size="sm">{review.author.name}</Heading>
+                            <Stars size={16} rating={review.rating} />
+                          </HStack>
+                          <Text>{review.text}</Text>
+                        </Stack>
+                      </Flex>
+                    ),
+                )
+              )}
+            </Stack>
+          </Box>
+        </Stack>
+
+        <BookStay
+          costPerNight={costPerNight}
+          bookings={bookings.filter(Boolean)}
+          listingId={id}
+          refetchQueries={[LISTING, { query: GUEST_TRIPS }]}
+          userRole={user?.__typename}
+        />
+      </Flex>
+    </Stack>
   );
 }
