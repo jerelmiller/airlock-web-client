edb2b47b5ebe57266f8278cfe1dc4b5bcd42d593
Jerel Miller <jerelmiller@gmail.com>
2024-08-28 16:07:35 -0600

    Remove noNav prop in Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -4,19 +4,17 @@ import { Container, ContainerProps } from "@chakra-ui/react";
 
 interface LayoutProps extends ContainerProps {
   children?: ReactNode;
-  noNav?: boolean;
   containerSize?: string;
 }
 
 export default function Layout({
-  noNav,
   children,
   containerSize = "container.xl",
   ...props
 }: LayoutProps) {
   return (
     <>
-      {!noNav && <Nav />}
+      <Nav />
       <Container maxW={containerSize} {...props} mb={24}>
         {children}
       </Container>
