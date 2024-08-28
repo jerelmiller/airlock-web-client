ff79748a0fa0baf4c929fb97382c51d63105c8a4
Jerel Miller <jerelmiller@gmail.com>
2024-08-28 16:45:31 -0600

    Add suspense and error boundary to Layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,20 @@
 import { Container } from "@chakra-ui/react";
+import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
+import { PageSpinner } from "../components/PageSpinner";
+import { ErrorBoundary } from "react-error-boundary";
+import { PageError } from "../components/PageError";
 
 export default function Layout() {
   return (
     <Container maxW="container.xl" mb={24}>
-      <Outlet />
+      <Suspense fallback={<PageSpinner />}>
+        <ErrorBoundary
+          fallbackRender={({ error }) => <PageError error={error} />}
+        >
+          <Outlet />
+        </ErrorBoundary>
+      </Suspense>
     </Container>
   );
 }
