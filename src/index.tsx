13fc4a3830cdc60baeac5698237d643fe46afbea
Jerel Miller <jerelmiller@gmail.com>
2024-08-29 08:11:13 -0600

    Reorganize imports

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,14 @@ import App from "./App";
 import { createRoot } from "react-dom/client";
 import { ApolloProvider } from "@apollo/client";
 import { loadDevMessages, loadErrorMessages } from "@apollo/client/dev";
+import theme from "./theme.js";
+import { ChakraProvider } from "@chakra-ui/react";
+import { client } from "./apolloClient";
 
 if (import.meta.env.DEV) {
   loadDevMessages();
   loadErrorMessages();
 }
-
-import theme from "./theme.js";
-import { ChakraProvider } from "@chakra-ui/react";
-import { client } from "./apolloClient";
 
 createRoot(document.getElementById("root")!).render(
   <ChakraProvider theme={theme}>
