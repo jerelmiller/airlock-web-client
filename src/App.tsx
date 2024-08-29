ae623c07fc824c849cad0f68c09e62f62ac49795
Jerel Miller <jerelmiller@gmail.com>
2024-08-29 09:54:36 -0600

    Wrap the app in a suspense boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./router";
+import { Suspense } from "react";
+import { PageSpinner } from "./components/PageSpinner";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<PageSpinner />}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
 }
