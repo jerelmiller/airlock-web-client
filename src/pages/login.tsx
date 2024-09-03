29a8e89317fd886e1fc19fad2070894217b9c6b9
Jerel Miller <jerelmiller@gmail.com>
2024-09-03 14:38:46 -0600

    Remove unneeded fragment for login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -59,147 +59,134 @@ export default function Login() {
   }
 
   return (
-    <>
-      <Container maxW="container.md">
-        <Stack spacing={6} alignItems="center">
-          <Heading as="h1" size="xl">
-            Choose a user role
-          </Heading>
-          <HStack spacing={6}>
-            <Box
-              border="1px solid"
-              borderRadius="8"
-              borderColor="gray.100"
-              p="6"
-            >
-              <Center h="300px">
-                <Flex alignItems="center" w="330px" direction="column">
-                  <Heading as="h2" size="lg">
-                    Host
-                  </Heading>
-                  <Stack mt="4">
-                    <HStack alignItems="center">
-                      <IoCheckmark /> <Text>Create and manage listings</Text>
-                    </HStack>
-                    <HStack alignItems="center">
-                      <IoCheckmark /> <Text>Edit your profile</Text>
-                    </HStack>
-                    <HStack alignItems="center">
-                      <IoCheckmark />{" "}
-                      <Text>Write reviews about your guests</Text>
-                    </HStack>
-                  </Stack>
-                  <Button
-                    as={Link}
-                    to="/"
-                    flex="1"
-                    mt="8"
-                    onClick={() => {
-                      login(HOST_USER);
-                    }}
-                    w="100%"
-                  >
-                    Log in as host
-                  </Button>
-                </Flex>
-              </Center>
-            </Box>
-            <Box
-              border="1px solid"
-              borderRadius="8"
-              borderColor="gray.100"
-              p="6"
-            >
-              <Center h="300px">
-                <Flex alignItems="center" w="330px" direction="column">
-                  <Heading as="h2" size="lg">
-                    Guest
-                  </Heading>
-                  <Stack mt="4">
-                    <HStack alignItems="center">
-                      <IoCheckmark />
-                      <Text>Book places to stay</Text>
-                    </HStack>
-                    <HStack alignItems="center">
-                      <IoCheckmark />
-                      <Text>Add funds to your wallet</Text>
-                    </HStack>
-                    <HStack alignItems="center">
-                      <IoCheckmark />
-                      <Text>Write reviews about your stay</Text>
-                    </HStack>
-                  </Stack>
-                  <Button
-                    as={Link}
-                    to="/"
-                    flex="1"
-                    onClick={() => {
-                      login(GUEST_USER);
-                    }}
-                    mt="8"
-                    w="100%"
-                  >
-                    Log in as guest
-                  </Button>
-                </Flex>
-              </Center>
-            </Box>
-          </HStack>
-          <Spacer mt={16} />
-          <Accordion allowToggle width="500px">
-            <AccordionItem borderTop={0} borderBottom={0}>
-              {({ isExpanded }) => (
-                <>
-                  <AccordionButton
-                    _hover={{ bgColor: "white" }}
-                    color="indigo.dark"
-                  >
-                    <Flex flex="1" alignItems="center" justifyContent="center">
-                      <Button variant="link" mr={2}>
-                        More login options
-                      </Button>
-                      {isExpanded ? <IoChevronUp /> : <IoChevronDown />}
-                    </Flex>
-                  </AccordionButton>
-                  <AccordionPanel py={4}>
-                    <Text fontSize="md" color="gray.600" mb={2}>
-                      Want to test out other accounts? Choose a user from the
-                      list!
-                    </Text>
-                    <Select
-                      onChange={handleChange}
-                      placeholder="Select a user"
-                      value={value}
-                    >
-                      {users.map((userId, index) => (
-                        <option key={index + 3} value={`user-${index + 3}`}>
-                          {userId}
-                        </option>
-                      ))}
-                    </Select>
-                    <Button
-                      as={Link}
-                      to="/"
-                      onClick={(e) => {
-                        if (value) {
-                          return login(value);
-                        }
-
-                        e.preventDefault();
-                      }}
-                      isDisabled={!value}
-                      mt={4}
-                      w="100%"
-                    >
-                      Log in
+    <Container maxW="container.md">
+      <Stack spacing={6} alignItems="center">
+        <Heading as="h1" size="xl">
+          Choose a user role
+        </Heading>
+        <HStack spacing={6}>
+          <Box border="1px solid" borderRadius="8" borderColor="gray.100" p="6">
+            <Center h="300px">
+              <Flex alignItems="center" w="330px" direction="column">
+                <Heading as="h2" size="lg">
+                  Host
+                </Heading>
+                <Stack mt="4">
+                  <HStack alignItems="center">
+                    <IoCheckmark /> <Text>Create and manage listings</Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <IoCheckmark /> <Text>Edit your profile</Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <IoCheckmark /> <Text>Write reviews about your guests</Text>
+                  </HStack>
+                </Stack>
+                <Button
+                  as={Link}
+                  to="/"
+                  flex="1"
+                  mt="8"
+                  onClick={() => {
+                    login(HOST_USER);
+                  }}
+                  w="100%"
+                >
+                  Log in as host
+                </Button>
+              </Flex>
+            </Center>
+          </Box>
+          <Box border="1px solid" borderRadius="8" borderColor="gray.100" p="6">
+            <Center h="300px">
+              <Flex alignItems="center" w="330px" direction="column">
+                <Heading as="h2" size="lg">
+                  Guest
+                </Heading>
+                <Stack mt="4">
+                  <HStack alignItems="center">
+                    <IoCheckmark />
+                    <Text>Book places to stay</Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <IoCheckmark />
+                    <Text>Add funds to your wallet</Text>
+                  </HStack>
+                  <HStack alignItems="center">
+                    <IoCheckmark />
+                    <Text>Write reviews about your stay</Text>
+                  </HStack>
+                </Stack>
+                <Button
+                  as={Link}
+                  to="/"
+                  flex="1"
+                  onClick={() => {
+                    login(GUEST_USER);
+                  }}
+                  mt="8"
+                  w="100%"
+                >
+                  Log in as guest
+                </Button>
+              </Flex>
+            </Center>
+          </Box>
+        </HStack>
+        <Spacer mt={16} />
+        <Accordion allowToggle width="500px">
+          <AccordionItem borderTop={0} borderBottom={0}>
+            {({ isExpanded }) => (
+              <>
+                <AccordionButton
+                  _hover={{ bgColor: "white" }}
+                  color="indigo.dark"
+                >
+                  <Flex flex="1" alignItems="center" justifyContent="center">
+                    <Button variant="link" mr={2}>
+                      More login options
                     </Button>
-                  </AccordionPanel>
-                </>
-              )}
-            </AccordionItem>
-          </Accordion>
-        </Stack>
-      </Container>
-    </>
+                    {isExpanded ? <IoChevronUp /> : <IoChevronDown />}
+                  </Flex>
+                </AccordionButton>
+                <AccordionPanel py={4}>
+                  <Text fontSize="md" color="gray.600" mb={2}>
+                    Want to test out other accounts? Choose a user from the
+                    list!
+                  </Text>
+                  <Select
+                    onChange={handleChange}
+                    placeholder="Select a user"
+                    value={value}
+                  >
+                    {users.map((userId, index) => (
+                      <option key={index + 3} value={`user-${index + 3}`}>
+                        {userId}
+                      </option>
+                    ))}
+                  </Select>
+                  <Button
+                    as={Link}
+                    to="/"
+                    onClick={(e) => {
+                      if (value) {
+                        return login(value);
+                      }
+
+                      e.preventDefault();
+                    }}
+                    isDisabled={!value}
+                    mt={4}
+                    w="100%"
+                  >
+                    Log in
+                  </Button>
+                </AccordionPanel>
+              </>
+            )}
+          </AccordionItem>
+        </Accordion>
+      </Stack>
+    </Container>
   );
 }
