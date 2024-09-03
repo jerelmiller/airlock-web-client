bd361b72e35044cf9fd6f420c4cb68a62fd1b63f
Jerel Miller <jerelmiller@gmail.com>
2024-09-03 11:57:53 -0600

    Add opacity when loading

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,18 +1,22 @@
 import { Container } from "@chakra-ui/react";
 import { Suspense } from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import { PageSpinner } from "../components/PageSpinner";
 import { ErrorBoundary } from "react-error-boundary";
 import { PageError } from "../components/PageError";
 
 export default function Layout() {
+  const { state } = useNavigation();
+
   return (
     <Container maxW="container.xl" mb={24}>
       <Suspense fallback={<PageSpinner />}>
         <ErrorBoundary
           fallbackRender={({ error }) => <PageError error={error} />}
         >
-          <Outlet />
+          <div style={{ opacity: state === "loading" ? 0.5 : 1 }}>
+            <Outlet />
+          </div>
         </ErrorBoundary>
       </Suspense>
     </Container>
